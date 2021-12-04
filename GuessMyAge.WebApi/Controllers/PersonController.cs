5f7f8ddecd6cb47d7a9e950886645827e4607b9b
dimiltone <dimiltone@gmail.com>
2021-12-04 17:35:21 +0100

    Ajout PersonController + GetAll

---
+++ GuessMyAge.WebApi/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using GuessMyAge.Business.Models;
+using GuessMyAge.Business.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuessMyAge.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonService personService;
+        public PersonController()
+        {
+            personService = new PersonService();
+        }
+
+        [HttpGet("")]
+        public IEnumerable<Person> GetAll()
+        {
+            return personService.GetAll();
+        }
+    }
+}
