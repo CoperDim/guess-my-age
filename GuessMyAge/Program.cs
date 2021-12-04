4a8d035ee24b3218d9edbce0491297e9d1a9be9a
dimiltone <dimiltone@gmail.com>
2021-12-04 13:25:15 +0100

    While UserInput

--- GuessMyAge/Program.cs
+++ GuessMyAge/Program.cs
@@ -1,26 +1,15 @@
-﻿string userName = Environment.UserName;
+﻿// See https://aka.ms/new-console-template for more information
+string userName = Environment.UserName;
 
 Console.WriteLine("Bienvenue " + userName);
-int currentMonth = DateTime.Now.Month;
 
-switch (currentMonth)
+string age = "21";
+string? userInput;
+
+do
 {
-    case 1:
-    case 2:
-    case 3:
-        Console.WriteLine("Premier trimestre");
-        break;
-    case 4:
-    case 5:
-    case 6:
-        Console.WriteLine("Second trimestre");
-        break;
-    case 7:
-    case 8:
-    case 9:
-        Console.WriteLine("Troisième trimestre");
-        break;
-    default:
-        Console.WriteLine("Dernier trimestre");
-        break;
+    userInput = Console.ReadLine();
+
 }
+while (userInput != age);
+
