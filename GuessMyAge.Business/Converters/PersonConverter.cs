c3e701152535064ab4f125581396c0bd2f220030
CopperDim <d.balderacchi@coperbee.fr>
2021-12-06 07:03:14 +0100

    StartingCode

---
+++ GuessMyAge.Business/Converters/PersonConverter.cs
@@ -0,0 +1,17 @@
+﻿using GuessMyAge.Database.Entities;
+using GuessMyAge.Models;
+
+namespace GuessMyAge.Business.Converters
+{
+    public static class PersonConverter
+    {
+        public static Person ToPerson(this PersonDatabaseEntity databaseEntity)
+        {
+            return new Person(databaseEntity.BirthDate,
+                                databaseEntity.Name, 
+                                databaseEntity.Job, 
+                                databaseEntity.Genre, 
+                                databaseEntity.Description);
+        }
+    }
+}
--- GuessMyAge.Models/Person.cs
+++ GuessMyAge.Models/Person.cs
@@ -2,10 +2,12 @@
 {
     public class Person
     {
+        public int Id { get; private set; }
         public int Age { get; private set; }
         public string Description { get; private set; }
         public string Name { get; private set; }
         public string Job { get; private set; }
+        public DateTime BirthDate { get; private set; }
 
         public Genre Genre { get; private set; }
 
@@ -15,7 +17,19 @@
             Description = description;
             Name = name;
             Job = job;
-            Genre = genre;
+        }
+
+        public Person(DateTime birthDate, string name, string job, string genre, string description, int? id = null)
+        {
+            Id = id ?? 0;
+            Age = new DateTime(DateTime.Now.Subtract(birthDate).Ticks).Year - 1;
+            BirthDate = birthDate;
+            Description = description;
+            Name = name;
+            Job = job;
+            Genre parsedGenre = Genre.Man;
+            Enum.TryParse(genre, out parsedGenre);
+            Genre = parsedGenre;
         }
     }
 }
